298f28e4fbe1a3125f5574a7a3ffaeac9bac63c8
Madhan Kumar <pingmadhankumar@gmail.com>
2018-11-29 02:07:32 +0530

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  IsMakeMoney: true,
+  IsMakeMoney: false,
   Title: "This is the title",
   Domain: "cool.com",
   TfnNumbers: "18002742129",
@@ -9,20 +9,21 @@ export const environment = {
   AddressLine2: "Raja Park, Jaipur - 302004",
   ContactNumber: "9940311034",
   CTATitle: "Quick Customer Support Number",
+  MapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3577.9888773207504!2d73.01093061541665!3d26.26202439411884!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39418c3e4389fdb9%3A0x6d6ad81a811317fb!2sNew+Power+House+Rd%2C+Jodhpur%2C+Rajasthan!5e0!3m2!1sen!2sin!4v1543373893514",
   companyLogo: {
     normal: "https://blog.theodo.fr/wp-content/uploads/2017/11/logo-theodo.png",
     money: "https://static.wixstatic.com/media/a8c8c1_bb994a1c86af44a4950cf17d235b73cc~mv2.jpg/v1/fill/w_201,h_45,al_c,q_80,usm_0.66_1.00_0.01/420980-quickbooks-desktop-enterprise-log.webp"
   },
   bannerImage: {
     normal: "https://scotch-res.cloudinary.com/image/upload/dpr_1,w_1050,q_auto:good,f_auto/media/1/XY1f5y4S4qEXPF38GDFm_deploying-angular-cli-apps-to-production-with-firebase.jpg",
-    money: "/assets/img/mn/banner.png"
+    money: "/banner.png"
   },
   producImage: {
     normal: "https://www.azocleantech.com/images/Article_Images/ImageForArticle_723(1).jpg",
     money: "../assets/img/imgbody.webp"
   },
   disclaimer: {
-    normal: "Disclaimer related to solar panel",
+    normal: "solar panel disclaimer",
     money: "We are an independent ProAdvisor firm to provide services for QuickBooks and other accounting software brands. Bookkeeping, accounting, payroll, tax filing and other related services offered by We are independent of Intuit Inc. We provide services through trained team of certified pro-advisors, certified accountants and experienced specialists. The brand-names, trademarks, logos, company names used in our website belong to their respective owners and are for informational purposes only."
   },
   CardDeckTitle: {

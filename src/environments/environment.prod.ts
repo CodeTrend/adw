417ad0433d02bac749d11f34a3775b2f8a57e480
Madhan Kumar <pingmadhankumar@gmail.com>
2018-12-02 00:12:50 +0530

    Cryovac

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,16 +3,16 @@ export const environment = {
   production: true,
   IsMakeMoney: true,
   Title: "This is the title",
-  Subdomain : "WeSachify",
+  Subdomain : "Cryovac",
   logoMaterialIcon: "cloud",
   Domain: ".firebaseapp.com",
   TfnNumbers: "1A8A0A2A3A2A1A0A1A11",
   TfnClickToCall: "1A-8A0A2-3A2A1-0A1A11",
-  AddressLine1: "3D Printers, Tribune Chowk,",
-  AddressLine2: " Industrial Area Phase II, Chandigarh, 160030",
-  ContactNumber: "9888999118",
+  AddressLine1: "Jeel Complex, Opp Bombay Hotel, Vijay Plot, Bhakti Nagar,",
+  AddressLine2: "Rajkot, Gujarat 360002",
+  ContactNumber: "9954787463",
   CTATitle: "Quick Customer Support Number",
-  MapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3430.7196781580083!2d76.78205611548256!3d30.6981622943482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fec8cc0000079%3A0x679b2a9b34f22c70!2s3D+Printers!5e0!3m2!1sen!2sin!4v1543597856874",
+  MapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3691.81776543222!2d70.79635091536805!3d22.28489194908452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3959ca122283738f%3A0x9be4a90721b15540!2sJeel+Complex%2C+Vijay+Plot+Main+Rd%2C+Opp+Bombay+Hotel%2C+Vijay+Plot%2C+Bhakti+Nagar%2C+Rajkot%2C+Gujarat+360002!5e0!3m2!1sen!2sin!4v1543688974956",
   companyLogo: {
     normal: "/assets/img/logo.png",
     money: "/assets/img/mn/logo.webp"
@@ -34,33 +34,33 @@ export const environment = {
     money: "Quickbooks"
   },
   CARD1: {
-    Title: "FDM 3D Printer - The Pramaan Series",
-    Body: "Global 3D Labs are the pioneers of 3D printing technology and leading manufacturers of 3D printers in India. Ever since our inception, we have aimed to be the front runners of 3D printing in India and are currently, on the verge of achieving that aim. We manufacture a wide range of best-in-class 3D printers and also, deliver coherent and reliable after-sales support. This makes Global 3D Labs the best and safest bet to buy 3D printers in India.",
-    ImageUrl: "https://5.imimg.com/data5/OR/EP/MY-9312288/fdm-3d-printer-global-3d-labs-500x500.png"
+    Title: "BOPP Multilayer Stretch Film, Length Per Roll (Meters):30-200",
+    Body: "Making of this range is done in accordance with the set industry norms and guidelines, utilizing the finest raw materials and modern machines. This ensures the product’s standard of quality. In addition to this, the offered range is known to be marked at the most reasonable rate possible.",
+    ImageUrl: "https://4.imimg.com/data4/XS/EY/MY-4373725/anti-fog-film-250x250.jpg"
   },
   CARD2: {
-    Title: "3D Printer Kit, For Personal And Office Use",
-    Body: "Rapid manufacturing/prototyping, Architecture, Art and Jewellery, Healthcare, Automotive, Industrial engineering, Education or Fashion With the new era of technology evolving 3D printers, we are well known on the 3D printing scene for the quality, durability and high level of control over the printing process. Based on these specifics users often choose our products to create functional prototypes, complex 3D models and other advanced 3D prints demanding rigorous quality standards.",
-    ImageUrl: "https://5.imimg.com/data5/AU/VQ/MY-66354358/delta-diy-3d-printer-500x500.jpg"
+    Title: "Chemical Coated Film",
+    Body: "AS Food Packaging, one of the leading Manufacturer, Exporter and Wholesaler of this highly commendable range of Sealing Machine, Food Packaging Box, Disposable Containers, Food Packaging Tray Cake Box,Biryani Box,Meal Tray,Disposable Thali,Plastic Food Container,Disposable Cutlery,Tissue paper,Aluminium container,Aluminium Foil,Aluminium container, ever since our establishment in the year 2010. ",
+    ImageUrl: "https://5.imimg.com/data5/KX/XB/MY-3438282/hand-grade-stretch-film-250x250.jpg"
   },
   CARD3: {
-    Title: "Flashforge Hunter DLP 3D Printer",
-    Body: "Incepted in the year of 2015, Printomake 3D Solutions Private Limited is one of the leading Wholesale traders, importer, Retailer of 3D Printer, 3D Printing Filament, 3D Printer Spare Parts and Service providers of 3D Printing Service,3D Modeling Service. We offer these to our customers at market leading rates.",
-    ImageUrl: "https://5.imimg.com/data5/WR/DJ/MY-41664526/1-500x500.jpg"
+    Title: "Golden Metallic Film",
+    Body: "Monarch is constructing incomparable quality products by using high quality of materials & components. We are manufacturing abundant range of packaging machines like Hand Sealers, Cap Sealers, Continuous Bag Sealer, Shrink Machine, Vacuum Packing Machines, Gas Flushing Machine, Paddle Sealer, L-Sealer Machine, Pallet Stretch Wrapper and many more.",
+    ImageUrl: "https://5.imimg.com/data5/NL/FK/MY-4874290/wrapping-film-roll-250x250.jpg"
   },
   CARD4: {
-    Title: "Triostar Prusa I3 Kit 3D Printer",
-    Body: "3D Printing Services due to proper completion in minimal time and with high accuracy. In addition to this, these presented products are easy to use and do not require maintenance up to several years. Light weighted these products are widely demanded in different commercial areas.",
-    ImageUrl: "https://5.imimg.com/data5/FK/NM/MY-3025760/creality-cr-10s-3d-printer-500x500.jpg"
+    Title: "Thermoforming Film",
+    Body: "The facility, parted into several highly operational units, helps us in the managing of the firm’s operations. The facility undergoes regular up-gradation, in order to maximize our presence in the market and to boost our production capacity. In addition to this, our ethical working habits have helped us generate a huge client base.",
+    ImageUrl: "https://5.imimg.com/data5/MS/OI/MY-709101/chemical-coated-films-250x250.jpg"
   },
   CARD5: {
-    Title: "White, Natural Plastic Carbon Fibre 3D Printer",
-    Body: "We satisfy clients with the help of our qualified team of professionals and advanced research and technology. Also, we are supported by an ultra-modern infrastructure. Moreover, our products meet the highest of quality standards in terms of their delivery of quality and performance as well as can be customized as demanded by the customers. ",
-    ImageUrl: "https://5.imimg.com/data5/GC/MB/MY-3025760/b-500x500.jpg"
+    Title: "Transparent Polyethylene POF Shrink Film",
+    Body: "The core competency of the company is in developing and producing customized machines and tools for flexible packaging applications. We believe in high degree of flexibility in project handling, innovative designs and customization. According to our philosophy, top performance can only be provided with continual further development of all operations. As a result, we regularly invest in new technologies and training of our employees.",
+    ImageUrl: "https://5.imimg.com/data5/AY/MO/MY-9806272/golden-metallized-film-250x250.jpg"
   },
   CARD6: {
-    Title: "CR-10S Creality 3D Printer",
-    Body: "All offered products are manufactured as per industry norms. In addition to this, our range has acknowledged due to the excellent features like highly efficient, easy usability, highly durable, excellent functionality, and optimum strength. Additionally, we are offering these products range at very reasonable rates. ",
-    ImageUrl: "https://5.imimg.com/data5/GC/MB/MY-3025760/b-500x500.jpg"
+    Title: "Cryovac Shrink Film",
+    Body: "We deal in varied industrial packaging machines such as shrink tunnel, L sealer, shrink chamber, semi / fully automatic carbon strapping machine, continuous base sealer and uniform/random carton sealer. Our products are known for their superior performance, easy operation, latest features, functions and compact designs.",
+    ImageUrl: "https://4.imimg.com/data4/DN/HH/MY-4559667/multilayer-stretch-films-for-printing-industry-250x250.jpg"
   }
 };
